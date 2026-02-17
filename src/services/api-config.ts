32e0a317c85029b9a16a9a344998d8d0ddfe46f8
OMKAR9395 <omkarmohite85@gmail.com>
2026-02-17 15:41:01 +0530

    api configuration

--- src/services/api-config.ts
+++ src/services/api-config.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 
-export const API_BASE_URL = 'https://localhost:7192/api';
-// export const API_BASE_URL = 'http://localhost:7192/api';
-export const API_RENDER_URL = 'https://displaytestingsoftwarebackend.onrender.com';
+// export const API_BASE_URL = 'https://localhost:7192/api';
+export const API_BASE_URL = 'http://localhost:7192/api';
+// export const API_RENDER_URL = 'https://displaytestingsoftwarebackend.onrender.com';
 
 @Injectable({
   providedIn: 'root',
